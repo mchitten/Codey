9d8218dfc3d4a6507c95d68d891fd7dc0f925f02
Michael Chittenden <mchitten@gmail.com>
2013-12-02 22:12:38 -0500

    So it begins.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Codey::Application.config.secret_key_base = 'eab1e68206f67ded089f868300642d682a2cff6088df5809b981ae6358933f7b6f5c8d212e3c6df3a5e82fddb43d7dc03f6a54fc4e894c0fb19ed2f1144af8d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Codey::Application.config.session_store :cookie_store, key: '_codey_session'
